0f00d9ca34cbf7da50b702494983e13e9367c5cc
HyperSniper89 <jakup.dam20@student.setur.fo>
2023-04-26 15:31:21 +0100

    Adding and deleting profiles fixed. moved new profiles to the bottom.

--- public/scripts.js
+++ public/scripts.js
@@ -1,28 +1,43 @@
+// Heintar employee data frá API
 async function fetchEmployeeData() {
+    // Sendur GET request til API
     const response = await fetch('/api/employees');
-    const employees = await response.json();
-    return employees;
+    // Setur employee data til at vera JSON
+    return await response.json();
 }
 
+// Sendur employee data til API
 async function postEmployeeData(employee) {
+
+    // Sendur POST request til API
     const response = await fetch('/api/employees', {
         method: 'POST',
         headers: {
+            // Setur header til at siga við API at data er JSON
             'Content-Type': 'application/json',
         },
+        // Setur body at innihalda employee data
         body: JSON.stringify(employee),
     });
-    const savedEmployee = await response.json();
-    return savedEmployee;
+
+    if (response.ok) {
+        return await response.json();
+    } else {
+        console.error(`Error posting employee data: ${response.statusText}`);
+    }
 }
 
+// Renderar employee profilar á heimasíðuna
 function renderEmployees(employees) {
     const employeeList = document.getElementById('employee-list');
     employeeList.innerHTML = '';
-
+    // Iterara gjøgnum employee array
     employees.forEach((employee) => {
+        // ger eitt div element fyri hvønn employee profil
         const profile = document.createElement('div');
+        // seta classa navn hjá profil div
         profile.className = 'employee-profile';
+        // Seta innara HTML innihald av profile div við employee data
         profile.innerHTML = `
             <img src="${employee.imageUrl}" alt="${employee.name}'s picture" width="100" height="100">
             <h3>${employee.name}</h3>
@@ -30,49 +45,72 @@ function renderEmployees(employees) {
             <p><strong>Biography:</strong> ${employee.bio}</p>
             <button class="delete-employee" data-id="${employee._id}">Delete</button>
         `;
+        //  ger delete button og vit gera ein event listener til knøttin at kalla á handleDeleteEmployee function
         const deleteButton = profile.querySelector('.delete-employee');
         deleteButton.addEventListener('click', handleDeleteEmployee);
-
+        // Appenda profil div til employee listan
         employeeList.appendChild(profile);
     });
 }
 
+// Handfara employee form submission
 async function handleEmployeeFormSubmit(e) {
+    // forða default form submission
     e.preventDefault();
 
+    // Samla input virði frá form fields
     const name = document.getElementById('employee-name').value;
     const role = document.getElementById('employee-role').value;
     const bio = document.getElementById('employee-bio').value;
     const imageUrl = document.getElementById('employee-image').value;
 
-    const newEmployee = {name, role, bio, imageUrl};
+    // ger eitt nýtt employee object frá form data
+    const newEmployee = { name, role, bio, imageUrl };
+
+    // Sendur employee object til API
     const savedEmployee = await postEmployeeData(newEmployee);
 
-    const employees = await fetchEmployeeData();
-    renderEmployees(employees);
+    // Heintar nýggja employee data frá API og rendera á heimasíðuna
+    renderEmployees([...(await fetchEmployeeData()), savedEmployee]);
 
     e.target.reset();
 }
 
+
+// Handfara delete employee button click
 async function handleDeleteEmployee(e) {
+    // Heinta employee id frá delete button
     const employeeId = e.target.dataset.id;
-    await deleteEmployeeData(employeeId);
-    const employees = await fetchEmployeeData();
-    renderEmployees(employees);
+    // Senda employee id til API
+    const isDeleted = await deleteEmployeeData(employeeId);
+    // Heinta nýggja employee lista frá API
+    if (isDeleted) {
+        const employees = await fetchEmployeeData();
+        // Rendera nýggja employee listan á heimasíðuna
+        renderEmployees(employees);
+    } else {
+        console.error('Error deleting employee');
+    }
 }
 
-async function deleteEmployeeData(id) {
-    const response = await fetch(`/api/employees/${id}`, {
+// Senda employee id til API fyri at sletta employee
+async function deleteEmployeeData(employeeId) {
+    // Senda DELETE request til API
+    const response = await fetch(`/api/employees/${employeeId}`, {
         method: 'DELETE',
     });
+
     return response.ok;
 }
 
-
+// Event listener til at handfara DOMContentLoaded event
 document.addEventListener('DOMContentLoaded', async () => {
+    // Heinta add employee data frá API
     const employeeForm = document.getElementById('add-employee-form');
+    // SetA event listener á form submission
     employeeForm.addEventListener('submit', handleEmployeeFormSubmit);
-
+    // Heinta employee data frá API
     const employees = await fetchEmployeeData();
+    // Rendera employee lista á heimasíðuna
     renderEmployees(employees);
 });
--- server.js
+++ server.js
@@ -1,65 +1,77 @@
-
+// Innlessur environment variablar frá .env file
 require('dotenv').config();
+// Importerar neyðug modul
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const { MongoClient } = require('mongodb');
+const { MongoClient, ObjectId } = require('mongodb');
 
+// Initialiserar Express app
 const app = express();
+// Setur port til 3000
 const PORT = process.env.PORT || 3000;
 
+// Setur upp middleware til at lesa JSON frá request body
 app.use(cors());
 app.use(bodyParser.json());
 
-// Connect to the MongoDB database
+// Setur upp MongoDB connection
 const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri);
 
-
+// Connect til MongoDB
 client.connect()
     .then(() => {
         console.log('Connected to MongoDB');
+        // Setur collection til at vera employees
+        const employeesCollection = client.db('ITConsultantBusiness').collection('employees');
 
-    const employeesCollection = client.db('ITConsultantBusiness').collection('employees');
-
-    // Get all employees
+    // Heinta allar employees
     app.get('/api/employees', async (req, res) => {
+        // eomployeesCollection.find() returnar eitt cursor object,.toArray() ger object til array. await er neyðugt til at gera async
         const employees = await employeesCollection.find().toArray();
+        // Sendur employees array til client
         res.send(employees);
     });
 
-    // Add a new employee
-    app.post('/api/employees', async (req, res) => {
-        const employee = req.body;
-        const result = await employeesCollection.insertOne(employee);
-        if (result.result.ok === 1) {
-            res.send({_id: result.insertedId});
-        } else {
-            res.sendStatus(500);
-        }
-    });
+        // Ger eitt nýtt employee object
+        app.post('/api/employees', async (req, res) => {
+            // Setur employee object frá request body
+            const employee = req.body;
+            // Setur employee object inn í employees collection
+            const result = await employeesCollection.insertOne(employee);
 
-    // Delete an employee
-    app.delete('/api/employees/:id', async (req, res) => {
-        const id = req.params.id;
-        const {ObjectId} = require('mongodb');
-        const result = await employeesCollection.deleteOne({_id: ObjectId(id)});
+            // Sendur aftur employee object við _id
+            if (result.insertedCount === 1) {
+                res.send({_id: result.insertedId});
+            } else {
+                res.sendStatus(500);
+            }
+        });
 
-        if (result.deletedCount === 1) {
-            res.sendStatus(200);
-        } else {
-            res.sendStatus(500);
-        }
-    });
+        // Sletta employee frá database
+        app.delete('/api/employees/:id', async (req, res) => {
+            // Setur employee id frá request params
+            const employeeId = req.params.id;
+            // Slettar employee frá employees collection
+            const result = await employeesCollection.deleteOne({ _id: new ObjectId(employeeId) });
+
+            if (result.deletedCount === 1) {
+                res.sendStatus(200);
+            } else {
+                res.sendStatus(500);
+            }
+        });
 
 
-
-    app.use(express.static('public'));
-    // Start the server
+    // Setur static files til at vera í public mappuni
+        app.use(express.static('public'));
+    // Startar servaran
     app.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`);
     });
     })
+    // Catchar error og stoppar serverarna - kanska broyta til at restarta server ístaðin!
     .catch((err) => {
         console.error('MongoDB connection error:', err);
         process.exit(1);
